bde3edf8bee602339f22dfaacae3dfc969fe5257
Henry Post <HenryFBP@gmail.com>
2021-01-16 17:35:34 -0600

    add 1 repo

--- windows-10/first-setup.ps1
+++ windows-10/first-setup.ps1
@@ -7,6 +7,7 @@ mkdir ~/Github
 cd ~/Github
 git clone https://github.com/HenryFBP/VagrantPackerFiles
 git clone https://github.com/HenryFBP/iso-notes
+git clone https://github.com/HenryFBP/examples
 git clone https://github.com/HenryFBP/discord-with-free-emojis
 git clone https://github.com/HenryFBP/ms-office-easy-install
 git clone https://github.com/HenryFBP/KMS-activator
