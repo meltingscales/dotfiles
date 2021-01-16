c0a46b9317c4c234cdfad1de2ae23ae8a7801233
Henry Post <HenryFBP@gmail.com>
2021-01-16 17:34:17 -0600

    make w10 first setup

---
+++ windows-10/first-setup.ps1
@@ -0,0 +1,14 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+choco install -y git
+
+
+mkdir ~/Github
+cd ~/Github
+git clone https://github.com/HenryFBP/VagrantPackerFiles
+git clone https://github.com/HenryFBP/iso-notes
+git clone https://github.com/HenryFBP/discord-with-free-emojis
+git clone https://github.com/HenryFBP/ms-office-easy-install
+git clone https://github.com/HenryFBP/KMS-activator
+
+choco install -y gimp steam libreoffice notepadplusplus 7zip git virtualbox vagrant python3 ruby
