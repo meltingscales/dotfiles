3d45b3816579c9ae6af07fa0ea5c423f3239527a
Henry Post <HenryFBP@gmail.com>
2021-01-16 18:02:15 -0600

    add and separate tools

--- windows-10/first-setup-part-2.ps1
+++ windows-10/first-setup-part-2.ps1
@@ -8,4 +8,12 @@ git clone https://github.com/HenryFBP/discord-with-free-emojis
 git clone https://github.com/HenryFBP/ms-office-easy-install
 git clone https://github.com/HenryFBP/KMS-activator
 
-choco install -y gimp steam libreoffice notepadplusplus 7zip git virtualbox vagrant python3 ruby chromium opera filefox inkscape
+choco install -y steam multimc 
+
+choco install -y chromium opera firefox vlc spotify
+
+choco install -y 7zip hxd vscode sysinternals curl wget windirstat
+
+choco install -y gimp libreoffice notepadplusplus inkscape audacity kdenlive obs-studio
+
+choco install -y virtualbox vagrant python3 ruby jdk8 nodejs wireshark 
