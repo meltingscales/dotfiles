4347ade80b1f6b2cdb337047e10ca58554097adc
Henry Post <HenryFBP@gmail.com>
2021-01-23 16:25:09 -0600

    add emus and comments for tool type sections

--- windows-10/first-setup-part-2.ps1
+++ windows-10/first-setup-part-2.ps1
@@ -1,7 +1,7 @@
 # Separated because `git` doesn't show up in PATH for some reason after `choco` install
 
-mkdir ~/Github
-cd ~/Github
+New-Item ~/Github
+Set-Location ~/Github
 git clone https://github.com/Sycnex/Windows10Debloater
 git clone https://github.com/HenryFBP/VagrantPackerFiles
 git clone https://github.com/HenryFBP/iso-notes
@@ -10,12 +10,17 @@ git clone https://github.com/HenryFBP/discord-with-free-emojis
 git clone https://github.com/HenryFBP/ms-office-easy-install
 git clone https://github.com/HenryFBP/KMS-activator
 
-choco install -y steam multimc 
+# games
+choco install -y steam multimc visualboyadvance zsnes dosbox retroarch dolphin scummvm ds4windows cemu #yuzu
 
+# multimedia
 choco install -y chromium opera firefox vlc spotify
 
-choco install -y 7zip hxd vscode sysinternals curl wget windirstat
-
+# editing tools
 choco install -y gimp libreoffice notepadplusplus inkscape audacity kdenlive obs-studio
 
+# small little coding/etc tools
+choco install -y 7zip hxd vscode sysinternals curl wget windirstat alacritty
+
+# code frameworks/large code tools
 choco install -y virtualbox vagrant python3 ruby jdk8 nodejs wireshark 
