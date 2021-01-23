b5acb8650263682d6498a9373460c5f7e1cac581
Henry Post <HenryFBP@gmail.com>
2021-01-23 16:36:54 -0600

    googledrive is broken, remove it

--- windows-10/first-setup-part-2.ps1
+++ windows-10/first-setup-part-2.ps1
@@ -22,7 +22,7 @@ choco install -y chromium opera firefox vlc spotify
 choco install -y gimp libreoffice notepadplusplus inkscape audacity kdenlive obs-studio
 
 # small little coding/etc tools
-choco install -y 7zip hxd vscode sysinternals curl wget windirstat alacritty googledrive
+choco install -y 7zip hxd vscode sysinternals curl wget windirstat alacritty
 
 # code frameworks/large code tools
 choco install -y virtualbox vagrant python3 ruby jdk8 nodejs wireshark 
