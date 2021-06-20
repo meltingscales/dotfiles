be0f787562bc415dded2b6f4c65e109e034f508d
Henry Post <henryfbp@gmail.com>
2021-06-20 14:28:46 -0500

    add hwidgen src

--- windows-10/first-setup-part-2.ps1
+++ windows-10/first-setup-part-2.ps1
@@ -1,14 +1,15 @@
 # Separated because `git` doesn't show up in PATH for some reason after `choco` install
 
 mkdir ~/Github
-cd ~/Github
+Set-Location ~/Github
+git clone https://github.com/HenryFBP/KMS-activator
+git clone https://github.com/HenryFBP/HWIDGEN-SRC
 git clone https://github.com/Sycnex/Windows10Debloater
 git clone https://github.com/HenryFBP/VagrantPackerFiles
 git clone https://github.com/HenryFBP/iso-notes
 git clone https://github.com/HenryFBP/examples
 git clone https://github.com/HenryFBP/discord-with-free-emojis
 git clone https://github.com/HenryFBP/ms-office-easy-install
-git clone https://github.com/HenryFBP/KMS-activator
 
 choco install -y steam multimc 
 
