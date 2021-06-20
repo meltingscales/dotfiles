881193912aee28a89080fb7bf87b8eacad9c2278
Henry Post <henryfbp@gmail.com>
2021-06-20 16:17:45 -0500

    split git and choco install commands

--- windows-10/first-setup-part-2.ps1
+++ windows-10/first-setup-part-2.ps1
@@ -10,20 +10,3 @@ git clone https://github.com/HenryFBP/VagrantPackerFiles
 git clone https://github.com/HenryFBP/examples
 git clone https://github.com/HenryFBP/discord-with-free-emojis
 git clone https://github.com/HenryFBP/ms-office-easy-install
-
-# games
-choco install -y steam battle.net multimc visualboyadvance zsnes dosbox dolphin scummvm ds4windows cemu 
-# yuzu DNE
-# retroarch hangs
-
-# multimedia
-choco install -y chromium opera firefox vlc spotify discord
-
-# editing tools
-choco install -y gimp libreoffice notepadplusplus inkscape audacity kdenlive obs-studio 
-
-# small little coding/etc tools
-choco install -y 7zip hxd vscode sysinternals curl wget windirstat alacritty cpu-z hwinfo openhardwaremonitor prime95 furmark intel-ipdt jetbrainstoolbox teamviewer
-
-# code frameworks/large code tools
-choco install -y virtualbox vagrant python3 ruby jdk8 nodejs wireshark partitionwizard
---
+++ windows-10/first-setup-part-3.ps1
@@ -0,0 +1,16 @@
+# games
+choco install -y steam battle.net multimc visualboyadvance zsnes dosbox dolphin scummvm ds4windows cemu 
+# yuzu DNE
+# retroarch hangs
+
+# multimedia
+choco install -y chromium opera firefox vlc spotify discord
+
+# editing tools
+choco install -y gimp libreoffice notepadplusplus inkscape audacity kdenlive obs-studio 
+
+# small little coding/etc tools
+choco install -y 7zip hxd vscode sysinternals curl wget windirstat alacritty cpu-z hwinfo openhardwaremonitor prime95 furmark intel-ipdt jetbrainstoolbox teamviewer
+
+# code frameworks/large code tools
+choco install -y virtualbox vagrant python3 ruby jdk8 nodejs wireshark partitionwizard
