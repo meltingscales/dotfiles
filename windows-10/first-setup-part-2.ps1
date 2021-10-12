060786b872432ade60f02aac25042fe9ff270984
Henry Post <HenryFBP@gmail.com>
2021-10-12 10:51:37 -0500

    Update first-setup-part-2.ps1

--- windows-10/first-setup-part-2.ps1
+++ windows-10/first-setup-part-2.ps1
@@ -3,8 +3,10 @@
 New-Item ~/Git
 Set-Location ~/Git
 git clone https://github.com/HenryFBP/KMS-activator
+git clone https://github.com/HenryFBP/dotfiles --recursive
 git clone https://github.com/HenryFBP/HWIDGEN-SRC
 git clone https://github.com/HenryFBP/iso-notes
+git clone https://github.com/HenryFBP/pingstats
 git clone https://github.com/Sycnex/Windows10Debloater #TODO W10 Emiliorated edition
 git clone https://github.com/HenryFBP/VagrantPackerFiles
 git clone https://github.com/HenryFBP/examples
