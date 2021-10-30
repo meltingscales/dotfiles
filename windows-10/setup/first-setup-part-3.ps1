a1e74f938cf29ccd97661679cd014489fae8d4f8
Henry Post <HenryFBP@gmail.com>
2021-10-30 14:20:31 -0500

    add samsung-magician

--- windows-10/setup/first-setup-part-3.ps1
+++ windows-10/setup/first-setup-part-3.ps1
@@ -10,14 +10,14 @@ choco install -y chromium opera firefox vlc discord
 choco install -y gimp libreoffice notepadplusplus inkscape audacity kdenlive obs-studio yed colorpic
 
 # small little technical tools
-choco install -y 7zip hxd sysinternals windirstat cpu-z hwinfo openhardwaremonitor prime95 furmark teamviewer bluescreenview dmde cyberduck <# ftp #> zeal deluge
+choco install -y 7zip hxd sysinternals windirstat cpu-z hwinfo openhardwaremonitor prime95 furmark teamviewer bluescreenview dmde cyberduck <# ftp #> zeal deluge samsung-magician  partitionwizard
 #  intel-ipdt DNW
 
 # coding tools but not IDEs
 choco install -y vscode curl wget alacritty jetbrainstoolbox snyk sonarqube-scanner.portable adb apktool
 
 # code frameworks/large code tools
-choco install -y virtualbox vagrant python3 ruby openjdk8 nodejs wireshark partitionwizard graphviz hugo-extended
+choco install -y virtualbox vagrant python3 ruby openjdk8 nodejs wireshark graphviz hugo-extended
 
 # npm utils
 npm install touch-cli -g
