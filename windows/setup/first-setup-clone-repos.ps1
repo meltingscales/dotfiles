59bda582aa797490747497ab86e6d043971205a8
henryfbp <henryfbp@gmail.com>
2021-11-26 21:41:02 -0500

    simplify repos

--- windows/setup/first-setup-clone-repos.ps1
+++ windows/setup/first-setup-clone-repos.ps1
@@ -1,3 +1,19 @@
+$repos='
+https://github.com/HenryFBP/KMS-activator
+https://github.com/HenryFBP/dotfiles
+https://github.com/HenryFBP/HWIDGEN-SRC
+https://github.com/HenryFBP/iso-notes
+https://github.com/HenryFBP/pingstats
+https://github.com/Sycnex/Windows10Debloater
+https://github.com/HenryFBP/VagrantPackerFiles
+https://github.com/HenryFBP/examples
+https://github.com/HenryFBP/discord-with-free-emojis
+https://github.com/HenryFBP/ms-office-easy-install
+https://github.com/viking-gps/viking
+';
+
+$repos=$repos.Split([Environment]::NewLine)
+
 # Separated because `git` doesn't show up in PATH for some reason after `choco` install
 
 if (!( Test-path ~/Git)) {
@@ -6,17 +22,12 @@ if (!( Test-path ~/Git)) {
 }
 
 Push-Location ~/Git 
- 
-git clone https://github.com/HenryFBP/KMS-activator
-git clone https://github.com/HenryFBP/dotfiles --recursive
-git clone https://github.com/HenryFBP/HWIDGEN-SRC
-git clone https://github.com/HenryFBP/iso-notes
-git clone https://github.com/HenryFBP/pingstats
-git clone https://github.com/Sycnex/Windows10Debloater #TODO W10 Emiliorated edition
-git clone https://github.com/HenryFBP/VagrantPackerFiles
-git clone https://github.com/HenryFBP/examples
-git clone https://github.com/HenryFBP/discord-with-free-emojis
-git clone https://github.com/HenryFBP/ms-office-easy-install
-git clone https://github.com/viking-gps/viking
+
+foreach ($s in $repos){
+    if($s){
+        git clone $s --recursive
+    }
+}
+
 
 Pop-Location
