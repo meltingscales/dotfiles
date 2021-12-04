d29ec757eb16d5fa8c2eb4e49168f10751337b8e
henryfbp <HenryFBP@gmail.com>
2021-12-03 20:51:42 -0500

    Revert "sync"
    This reverts commit b1da692e55c6c3dd1f8848380309cb6131d34360.

--- util/fetch-all-git-repos.ps1
+++ util/fetch-all-git-repos.ps1
@@ -4,7 +4,6 @@ foreach ($folder in (get-item "./*")){
     write-host "git fetch --all <= [$(split-path -path $folder -leaf)]"
     Push-Location $folder
     git fetch --all
-    git push --all
     Pop-Location
 }
 
