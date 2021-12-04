b1da692e55c6c3dd1f8848380309cb6131d34360
henryfbp <HenryFBP@gmail.com>
2021-12-03 20:43:26 -0500

    sync

--- util/sync-all-git-repos.ps1
+++ util/sync-all-git-repos.ps1
@@ -4,6 +4,7 @@ foreach ($folder in (get-item "./*")){
     write-host "git fetch --all <= [$(split-path -path $folder -leaf)]"
     Push-Location $folder
     git fetch --all
+    git push --all
     Pop-Location
 }
 
