02ae76dba0140bcb980a1fda41caaba96ed433ae
Henry Post <HenryFBP@gmail.com>
2021-12-07 18:20:11 -0500

    add gb display

--- windows/etc/get-disk-size.ps1
+++ windows/etc/get-disk-size.ps1
@@ -2,5 +2,8 @@ $diskname = 'C:';
 
 $disk = Get-WmiObject Win32_LogicalDisk -Filter "DeviceID='$diskname'" | Select-Object Size, FreeSpace
 
-write-host "Size: ", $disk.Size
-write-host "Free space: ", $disk.FreeSpace
+write-host "Size        in  B:  ", $disk.Size
+write-host "Free space  in  B:  ", $disk.FreeSpace
+
+write-host "Size        in GB:  ", ((($disk.Size/1000)/1000)/1000)
+write-host "Free space  in GB:  ", ((($disk.FreeSpace/1000)/1000)/1000)
