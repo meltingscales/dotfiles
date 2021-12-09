b2164dc3b687bd46264249c20b096bda56c05f28
Henry Post <HenryFBP@gmail.com>
2021-12-09 11:31:51 -0500

    start making actual data structure lol. also require admin

--- windows/setup/first-setup-install-packages.ps1
+++ windows/setup/first-setup-install-packages.ps1
@@ -1,11 +1,31 @@
+#Requires -RunAsAdministrator
 
 # numbers from testing this in a VM, as of 02ae76dba0140bcb980a1fda41caaba96ed433ae
-$size = (110.610341888 - 87.652098048).ToString("#.##");
+$sizeGB = (110.610341888 - 87.652098048).ToString("#.##");
 
-$confirmation = Read-Host "This script will take up $size GB... Continue? (y/n):"
+$confirmation = Read-Host "This script, if never run, will take up approx. $sizeGB GB... Continue? (y/n):"
 if (!($confirmation -eq 'y')) {
     throw "Aborted!"
 }
+
+# $PACKAGE_LIST=@{
+#     GAMES="
+# steam
+# battle.net
+# multimc
+# visualboyadvance
+# zsnes
+# dosbox
+# dolphin
+# scummvm
+# ds4windows
+# cemu"
+# }
+
+# write-host "test"
+# write-host $PACKAGE_LIST.GAMES.Split("`n")[2] # should be battle.net
+
+# # TODO finish this package list thing
 
 # games
 choco install -y steam battle.net multimc visualboyadvance zsnes dosbox dolphin scummvm ds4windows cemu 
@@ -19,7 +39,7 @@ choco install -y chromium opera firefox vlc discord
 choco install -y gimp libreoffice notepadplusplus inkscape audacity kdenlive obs-studio yed colorpic calibre adobereader
 
 # small little technical tools
-choco install -y 7zip hxd sysinternals windirstat cpu-z hwinfo openhardwaremonitor prime95 furmark teamviewer bluescreenview dmde cyberduck <# ftp #> zeal deluge samsung-magician partitionwizard grep emacs
+choco install -y 7zip hxd sysinternals windirstat cpu-z hwinfo openhardwaremonitor prime95 furmark teamviewer bluescreenview dmde cyberduck <# ftp #> zeal deluge samsung-magician partitionwizard grep emacs procexp
 #  intel-ipdt DNW
 
 # coding tools but not IDEs
