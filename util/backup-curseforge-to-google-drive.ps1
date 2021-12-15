f3884d0781d45a83131f0cfdc1c03d9854a210de
henryfbp <HenryFBP@gmail.com>
2021-12-14 22:43:05 -0500

    add curseforge backer-upperer

---
+++ util/backup-curseforge-to-google-drive.ps1
@@ -0,0 +1,13 @@
+$date = (Get-Date -Format "MM-dd-yyyy-HH-mm")
+$destfolder = "G:\My Drive\Backups\CurseForgeMinecraftAutoBackups"
+$dest = join-path -path "$destfolder" -childpath "curseforge-instances-$($date).zip"
+$src = "C:\Users\henryfbp\curseforge\minecraft\Instances\"
+
+if (!(Test-Path $destfolder)) {
+    new-item -Path $destfolder -ItemType "Directory"
+}
+
+compress-archive -path $src -DestinationPath $dest
+
+Write-Host "Your CurseForge modpack/world backup is done:"
+Write-Host "$dest"
