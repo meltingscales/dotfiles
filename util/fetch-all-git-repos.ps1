43c6e7f38a18ccc88ed58b44f38f2f01b61c19b8
Henry Post <HenryFBP@gmail.com>
2022-01-29 12:54:17 -0500

    only fetch repos + folders

--- util/fetch-all-git-repos.ps1
+++ util/fetch-all-git-repos.ps1
@@ -1,11 +1,21 @@
-Push-Location ~/Git/
+$gitdir="~/Git/"
 
-foreach ($folder in (get-item "./*")){
-    write-host "git fetch --all <= [$(split-path -path $folder -leaf)]"
+Push-Location $gitdir
+
+foreach ($folder in (Get-ChildItem -Directory $gitdir)){
+
+    $currFolder=$(split-path -path $folder -leaf)
+
     Push-Location $folder
-    git fetch --all
-    Pop-Location
-}
 
+    if(test-path "./.git/"){
+        write-host "git fetch --all <= [$currFolder]"
+            git fetch --all
+    } else {
+        write-host "Not a git repo  -- [$currFolder]"
+    }
+
+    Pop-Location    
+}
 
 Pop-Location
