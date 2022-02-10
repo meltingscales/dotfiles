7ea2cc0573917253e2f9f1680284797edae84de9
henryfbp <HenryFBP@gmail.com>
2022-02-10 13:16:05 -0500

    make startup name better

--- windows/startup.cmd
+++
@@ -1,3 +0,0 @@
-REM put a shortcut to this file in "~\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
-
-cmd /c start powershell -noexit ~/Git/dotfiles/windows/startup.ps1
---
+++ windows/windows-dotfiles-startup.cmd
@@ -0,0 +1,4 @@
+REM put this file in "~\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+REM then run "ms-settings:startup" in cmd to see if it shows up
+
+cmd /c start powershell -noexit ~/Git/dotfiles/windows/startup.ps1
