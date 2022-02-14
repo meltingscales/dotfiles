402412cdafe96172cd4610919191914a4cfe6a23
Henry Post <HenryFBP@gmail.com>
2022-02-14 10:44:23 -0500

    show instantnews on startup

--- windows/startup.ps1
+++ windows/startup.ps1
@@ -6,6 +6,25 @@ Invoke-Expression ~/Git/dotfiles/util/fetch-all-git-repos.ps1
 #     Invoke-Expression "python3 pull-bandcamp-wishlisted-albums-to-data.py" 
 # }
 
+write-host "Weather:"
 curl.exe https://wttr.in/
 
+write-host "News:"
+if(!($ENV:IN_API_KEY)){
+    write-host "You must set 'IN_API_KEY' env var for the 'instantnews' tool. Or restart shell if you already set it."
+} else {
+    $startInfo = New-Object 'System.Diagnostics.ProcessStartInfo' -Property @{
+        FileName = "instantnews"
+        Arguments = "--news", "google-news"
+        UseShellExecute = $false
+        RedirectStandardInput = $true
+    }
+    $process = [System.Diagnostics.Process]::Start($startInfo)
+    Start-Sleep -Seconds 5
+    $process.StandardInput.WriteLine("n")
+    # write-host $process
+    
+}
+
+write-host ""
 Write-Host "Done with startup! Enjoy your day :3c"
