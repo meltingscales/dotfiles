2a1425619645e969ea780d93026b722f5a472783
henryfbp <HenryFBP@gmail.com>
2022-02-20 10:02:59 -0500

    run git fetch in separate terminal

--- windows/startup.ps1
+++ windows/startup.ps1
@@ -1,33 +1,35 @@
-write-host "Welcome to HenryFBP/dotfiles/windows/startup.ps1 v1.0 :3c"
 
-Invoke-Expression ~/Git/dotfiles/util/fetch-all-git-repos.ps1
+function Show-News {
+    if (!($ENV:IN_API_KEY)) {
+        Write-Host "You must set 'IN_API_KEY' env var for the 'instantnews' tool. Or restart shell if you already set it."
+    }
+    else {
+        $startInfo = New-Object 'System.Diagnostics.ProcessStartInfo' -Property @{
+            FileName              = "instantnews"
+            Arguments             = "--news", "google-news"
+            UseShellExecute       = $false
+            RedirectStandardInput = $true
+        }
+        $process = [System.Diagnostics.Process]::Start($startInfo)
+        Start-Sleep -Seconds 5
+        $process.StandardInput.WriteLine("n")
+        # Write-Host $process
+        
+    }    
+}
 
-# if(Test-Path "~/Git/henryfbp.github.io/scripts/pull-bandcamp-albums/"){
-#     Push-Location "~/Git/henryfbp.github.io/scripts/pull-bandcamp-albums/"
-#     Invoke-Expression "python3 pull-bandcamp-purchased-albums-to-data.py"
-#     Invoke-Expression "python3 pull-bandcamp-wishlisted-albums-to-data.py" 
-# }
+Write-Host "    === Welcome to HenryFBP/dotfiles/windows/startup.ps1 v1.0 :3c ==="
+Write-Host ""
 
-write-host "Weather:"
+Write-Host "Updating Git repos in separate terminal..."
+Start-Sleep 1
+cmd.exe /c START powershell.exe Invoke-Expression ~/Git/dotfiles/util/fetch-all-git-repos.ps1
+
+Write-Host "Weather:"
 curl.exe https://wttr.in/
 
-write-host "News:"
-if (!($ENV:IN_API_KEY)) {
-    write-host "You must set 'IN_API_KEY' env var for the 'instantnews' tool. Or restart shell if you already set it."
-}
-else {
-    $startInfo = New-Object 'System.Diagnostics.ProcessStartInfo' -Property @{
-        FileName              = "instantnews"
-        Arguments             = "--news", "google-news"
-        UseShellExecute       = $false
-        RedirectStandardInput = $true
-    }
-    $process = [System.Diagnostics.Process]::Start($startInfo)
-    Start-Sleep -Seconds 5
-    $process.StandardInput.WriteLine("n")
-    # write-host $process
-    
-}
+Write-Host "News:"
+Show-News
 
-write-host ""
+Write-Host ""
 Write-Host "Done with startup! Enjoy your day :3c"
