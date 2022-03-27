7450a84acebcefa0ae202984dbaafc3ec6ceb99f
henryfbp <HenryFBP@gmail.com>
2022-03-27 15:57:28 -0400

    improve curseforge backup

--- util/backup-curseforge-to-google-drive.ps1
+++ util/backup-curseforge-to-google-drive.ps1
@@ -1,13 +1,31 @@
-$date = (Get-Date -Format "MM-dd-yyyy-HH-mm")
 $destfolder = "G:\My Drive\Backups\CurseForgeMinecraftAutoBackups"
-$dest = join-path -path "$destfolder" -childpath "curseforge-instances-$($date).zip"
 $src = resolve-path "~\curseforge\minecraft\Instances\"
+$dest = join-path -path "$destfolder" -childpath "curseforge-instances-$($date).zip"
+$includeBaseDirectory = $false
+$compressionLevel = [System.IO.Compression.CompressionLevel]::Optimal
+
+$date = (Get-Date -Format "MM-dd-yyyy-HH-mm")
 
 if (!(Test-Path $destfolder)) {
     new-item -Path $destfolder -ItemType "Directory"
 }
 
-compress-archive -path $src -DestinationPath $dest
+Write-Host "Compressing 
+    $src
+into
+    $dest
+..."
 
-Write-Host "Your CurseForge modpack/world backup is done:"
-Write-Host "$dest"
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+[System.IO.Compression.ZipFile]::CreateFromDirectory("$src", "$dest", $compressionLevel, $includeBaseDirectory)
+# Compress-Archive -path $src -DestinationPath $dest #don't use compress-archive, it runs out of memory
+
+if (!($?)) {
+    Write-Host "Something went wrong :("
+}
+else {
+
+    Write-Host "Your CurseForge modpack/world backup is done:"
+    Write-Host "$dest"
+}
+
