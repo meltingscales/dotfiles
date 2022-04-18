72ea4b4bc3112c2e1de47960d0a0e7a187bf958f
Henry Post <HenryFBP@gmail.com>
2022-04-18 09:31:39 -0400

    improve deb sh

--- nix/debian/install-apt-packages.sh
+++ nix/debian/install-apt-packages.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-sudo apt install -y \ 
-    tmux \
-    lynx \
-    wget \
-    curl \
-    htop \
-    virtualbox \
-    docker
+sudo apt install snap tmux lynx wget curl htop docker
+# virtualbox for debian 11? QwQ
+
+sudo snap install --classic powershell
+sudo snap install           discord
